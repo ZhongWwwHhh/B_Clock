8e1dc2a134790c0e7e4b0fe059e8ea057875d3a1
ZhongWwwHhh <zhongwwwhhh@outlook.com>
2023-10-20 23:45:23 +0800

    调整tim4计数量，实现准时响铃

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -715,7 +715,7 @@ static void MX_TIM4_Init(void)
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 719;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 999;
+  htim4.Init.Period = 900;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
